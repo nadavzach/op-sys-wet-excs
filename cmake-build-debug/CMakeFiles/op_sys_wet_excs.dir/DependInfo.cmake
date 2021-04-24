ecc72e2219256b61779f6f661b67dbc729eb47c9
compm-nadav <nadavzech@gmail.com>
2021-04-24 21:31:52 +0300

    test

---
+++ cmake-build-debug/CMakeFiles/op_sys_wet_excs.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/compm/CLionProjects/op-sys-wet-excs/commands.cpp" "/home/compm/CLionProjects/op-sys-wet-excs/cmake-build-debug/CMakeFiles/op_sys_wet_excs.dir/commands.cpp.o"
+  "/home/compm/CLionProjects/op-sys-wet-excs/signals.cpp" "/home/compm/CLionProjects/op-sys-wet-excs/cmake-build-debug/CMakeFiles/op_sys_wet_excs.dir/signals.cpp.o"
+  "/home/compm/CLionProjects/op-sys-wet-excs/smash.cpp" "/home/compm/CLionProjects/op-sys-wet-excs/cmake-build-debug/CMakeFiles/op_sys_wet_excs.dir/smash.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/op_sys_wet_excs.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/op_sys_wet_excs.dir/commands.cpp.o"
+  "CMakeFiles/op_sys_wet_excs.dir/signals.cpp.o"
+  "CMakeFiles/op_sys_wet_excs.dir/smash.cpp.o"
+  "op_sys_wet_excs"
+  "op_sys_wet_excs.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/op_sys_wet_excs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
